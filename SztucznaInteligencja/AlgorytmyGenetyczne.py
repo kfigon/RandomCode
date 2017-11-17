0aea8fba9c78245d3861b766c56da0c4b1236a76
Kamil <kamil.figon@gmail.com>
2017-11-17 13:00:27 +0100

    genetyka skonczona

--- SztucznaInteligencja/AlgorytmyGenetyczne.py
+++ SztucznaInteligencja/AlgorytmyGenetyczne.py
@@ -32,7 +32,7 @@ class Chromosom:
 
         return Chromosom(tab1)
 
-    # tablica!
+    # input - tablica
     def liczFitness(self, ideal):
         procentBledow = 0
         for i in range(len(ideal)):
@@ -56,7 +56,7 @@ def losujLitere():
     if(liczba == 0):
         liczba = 32 # spacja
     else:
-        liczba += 97 # a
+        liczba += 96 # a
     return chr(liczba)
 
 def str2Tab(napis):
@@ -67,19 +67,23 @@ def str2Tab(napis):
 
 
 class Populacja:
-    def __init__(self, liczebnosc, dlugoscChromosomu, cel, wspolczynnikMutacji=1):
+    def __init__(self, liczebnosc, cel, wspolczynnikMutacji=1):
         self.__wspolczynnikMutacji = wspolczynnikMutacji
         self.__cel = cel
+        dlugoscChromosomu = len(cel)
 
         self.__populacja = [None]*liczebnosc
         for i in range(len(self.__populacja)):
             self.__populacja[i] = Chromosom()
             self.__populacja[i].randomize(dlugoscChromosomu)
 
+    def getDlugoscChromosomU(self):
+        return len(self.__cel)
+
     def getLiczebnoscPopulacji(self):
         return len(self.__populacja)
 
-    def getFitness(self):
+    def liczFitnessKazdegoOsobnika(self):
         out = [0]*self.getLiczebnoscPopulacji()
         i=0
         for osobnik in self.__populacja:
@@ -88,7 +92,7 @@ class Populacja:
         return out
 
     def __liczSumeFitnessu(self):
-        fitnessPopulacji = self.getFitness()
+        fitnessPopulacji = self.liczFitnessKazdegoOsobnika()
         suma = 0
         for i in fitnessPopulacji:
             suma+=i
@@ -99,13 +103,92 @@ class Populacja:
         return suma/self.getLiczebnoscPopulacji()
 
     def wybierzXNajlepszych(self, ileWybrac=2):
-        sumaFitnessu = self.__liczSumeFitnessu()
-        procentowyUdzialFitnessu = self.getFitness()
-        for i in range(len(procentowyUdzialFitnessu)):
-            procentowyUdzialFitnessu[i] /= sumaFitnessu
-            procentowyUdzialFitnessu[i] *= 100
+        fitnesy = self.liczFitnessKazdegoOsobnika()
+        r = Ruletka(fitnesy)
 
-        zwyciezkiProcent = random.randint(0,100)
+        zwyciezcy =[]
+        for i in range(ileWybrac):
+            zwyciezkiProcent = random.randint(1,100)
+            zwyciezcy.append(r.wybierzIndeksZwyciezcy(zwyciezkiProcent))
+        return zwyciezcy
 
     def generujNowePokolenie(self):
-        pass
+        nowaPopulacja = [None]*self.getLiczebnoscPopulacji()
+        for i in range(len(nowaPopulacja)):
+            zwyciezcy = self.wybierzXNajlepszych(2)
+            rodzicA = self.__populacja[zwyciezcy[0]]
+            rodzicB =self.__populacja[zwyciezcy[1]]
+            idx = random.randint(1, self.getDlugoscChromosomU())
+            dziecko = rodzicA.krzyzuj(rodzicB, idx)
+            dziecko.mutuj(self.__wspolczynnikMutacji)
+            nowaPopulacja[i]=dziecko
+
+        self.__populacja.clear()
+        self.__populacja=nowaPopulacja
+
+    def piszPopulacje(self):
+        for i in self.__populacja:
+            print(str(i))
+
+    def getNajlepszy(self):
+        najlepszyIdx=0
+        for i in range(self.getLiczebnoscPopulacji()):
+            aktualnyOsobnik = self.__populacja[i]
+            najlepszyOsobnik = self.__populacja[najlepszyIdx]
+
+            if(najlepszyOsobnik.liczFitness(self.__cel) < aktualnyOsobnik.liczFitness(self.__cel)):
+                najlepszyIdx = i
+
+        return self.__populacja[najlepszyIdx]
+
+class Ruletka:
+    def __init__(self, tab):
+        self.__tab = tab
+        self.__suma = 0
+        for i in self.__tab:
+            self.__suma+=i
+
+    def getSuma(self):
+        return self.__suma
+
+    # wartosci od <1:100>
+    def wybierzIndeksZwyciezcy(self, val):
+        if(val < 1 or val > 100):
+            raise Exception("niewlasciwy parametr ruletki " + str(val))
+
+        suma = self.getSuma()
+        # przejscie z procentu na wartosc
+        wlasciwaWartosc = (val*suma)/100
+
+        v = self.__tab[0]
+        indexToReturn = 0
+        for i in range(1, len(self.__tab)):
+            if(wlasciwaWartosc <= v ):
+                return indexToReturn
+            indexToReturn+=1
+            v += self.__tab[i]
+
+        return indexToReturn
+
+
+if __name__=='__main__':
+    cel = "byc albo nie byc"
+    p = Populacja(500, str2Tab(cel), 5)
+
+    sredniFitness = p.liczSredniFitness()
+    fitness = sredniFitness
+    nrPopulacji=0
+    print("start")
+    najlepszy = p.getNajlepszy()
+    while(fitness < 100):
+        nrPopulacji+=1
+        p.piszPopulacje()
+
+        print("---->populacja nr %d, sredni fitness %d, najlepszy %d: %s" % (nrPopulacji, sredniFitness, fitness, str(najlepszy)))
+        sredniFitness = p.liczSredniFitness()
+        p.generujNowePokolenie()
+        najlepszy = p.getNajlepszy()
+        fitness = najlepszy.liczFitness(str2Tab(cel))
+
+    print("skonczylem, ile populacji: %d" % nrPopulacji)
+    print("najlepszy: %s" % (str(najlepszy)))
--- SztucznaInteligencja/TestyGenetyki.py
+++ SztucznaInteligencja/TestyGenetyki.py
@@ -56,18 +56,98 @@ class TestyPopulacji(unittest.TestCase):
         self.celTab = str2Tab(self.cel)
 
     def testLiczebnosci(self):
-        p = Populacja(1000, len(self.celTab),self.celTab, 1)
+        p = Populacja(1000, self.celTab, 1)
         self.assertEqual(1000, p.getLiczebnoscPopulacji())
 
     def testLiczebnosciPoGenerowaniuPokolenia(self):
-        p = Populacja(1000, len(self.celTab), 1)
+        p = Populacja(500, self.celTab, 1)
         p.generujNowePokolenie()
-        self.assertEqual(1000, p.getLiczebnoscPopulacji())
+        self.assertEqual(500, p.getLiczebnoscPopulacji())
 
-    def testRuletki(self):
-        # p = Populacja(5, len(self.celTab),self.celTab, 1)
-        # p.wybierzXNajlepszych(2)
-        self.fail('to do')
+
+class TestPopulacjiEndToEnd(unittest.TestCase):
+    def test(self):
+        cel = "asd"
+        celTab = str2Tab(cel)
+        p = Populacja(500, celTab, 1)
+        najlepszy = p.getNajlepszy()
+        fitnessNajlepszego = najlepszy.liczFitness(celTab)
+        nrPopulacji = 0
+        while(fitnessNajlepszego < 100):
+            p.generujNowePokolenie()
+            nrPopulacji+=1
+            najlepszy = p.getNajlepszy()
+            fitnessNajlepszego = najlepszy.liczFitness(celTab)
+
+        self.assertEqual(cel, str(najlepszy))
+        print('nr populacji %d' % nrPopulacji)
+        self.assertLess(nrPopulacji, 10)
+
+
+class TestRuletki(unittest.TestCase):
+    def setUp(self):
+        # kolejno procenty:
+        #    26  2  1  30  19  22, inkrementalnie:
+        #     0  26 28 29  59  78  100
+        tab=[70, 5, 3, 80, 50, 60]
+        self.r = Ruletka(tab)
+
+    def testNegatywny1(self):
+        try:
+            self.r.wybierzIndeksZwyciezcy(0)
+        except Exception as e:
+            pass
+        else:
+            self.fail("spodziewano sie wyjatku")
+
+    def testNegatywny2(self):
+        try:
+            self.r.wybierzIndeksZwyciezcy(-1)
+        except Exception as e:
+            pass
+        else:
+            self.fail("spodziewano sie wyjatku")
+
+    def testNegatywny3(self):
+        try:
+            self.r.wybierzIndeksZwyciezcy(101)
+        except Exception as e:
+            pass
+        else:
+            self.fail("spodziewano sie wyjatku")
+
+    def testLosowaniaIdx0(self):
+        self.assertEqual(0, self.r.wybierzIndeksZwyciezcy(1))
+        self.assertEqual(0, self.r.wybierzIndeksZwyciezcy(3))
+        self.assertEqual(0, self.r.wybierzIndeksZwyciezcy(5))
+        self.assertEqual(0, self.r.wybierzIndeksZwyciezcy(8))
+        self.assertEqual(0, self.r.wybierzIndeksZwyciezcy(10))
+        self.assertEqual(0, self.r.wybierzIndeksZwyciezcy(15))
+        self.assertEqual(0, self.r.wybierzIndeksZwyciezcy(20))
+        self.assertEqual(0, self.r.wybierzIndeksZwyciezcy(26))
+
+    def testLosowaniaIdx1(self):
+        self.assertEqual(1, self.r.wybierzIndeksZwyciezcy(27))
+
+    def testLosowaniaIdx2(self):
+        self.assertEqual(2, self.r.wybierzIndeksZwyciezcy(28))
+        self.assertEqual(2, self.r.wybierzIndeksZwyciezcy(29))
+
+    def testLosowaniaIdx3(self):
+        self.assertEqual(3, self.r.wybierzIndeksZwyciezcy(30))
+        self.assertEqual(3, self.r.wybierzIndeksZwyciezcy(35))
+        self.assertEqual(3, self.r.wybierzIndeksZwyciezcy(40))
+        self.assertEqual(3, self.r.wybierzIndeksZwyciezcy(50))
+
+    def testLosowaniaIdx4(self):
+        self.assertEqual(4, self.r.wybierzIndeksZwyciezcy(59))
+        self.assertEqual(4, self.r.wybierzIndeksZwyciezcy(60))
+
+    def testLosowaniaIdx5(self):
+        self.assertEqual(5, self.r.wybierzIndeksZwyciezcy(78))
+        self.assertEqual(5, self.r.wybierzIndeksZwyciezcy(79))
+        self.assertEqual(5, self.r.wybierzIndeksZwyciezcy(90))
+        self.assertEqual(5, self.r.wybierzIndeksZwyciezcy(100))
 
 if __name__ == '__main__':
     unittest.main()
