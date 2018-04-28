fc74c8da7ce97e1cb49b460a839a19262bb29fee
kfigon <kamil.figon@jitsolutions.pl>
2018-04-28 11:40:07 +0200

    minor app stuff

--- _WEB_DEV_/ProjectBlog/application.py
+++ _WEB_DEV_/ProjectBlog/application.py
@@ -3,10 +3,19 @@ from flask import Flask, render_template, request,jsonify
 
 app = Flask(__name__)
 
+def czytajPosty():
+    return [
+        {'tytul': 'Bylem wczoraj tutaj', 'tresc': 'bylo awesome!'},
+        {'tytul': 'Nowi avengersi', 'tresc': 'nie podobaja mi sie'},
+        {'tytul': 'taki sobie wpis', 'tresc': 'ale bez tresci...'}
+    ]
+
 @app.route('/')
 @app.route('/index')
 def dawajStart():
-    return render_template('index.html')
+    posty = czytajPosty()
+    return render_template('index.html',
+    posty=posty)
 
 if __name__=='__main__':
     app.run(debug=True)
--- _WEB_DEV_/ProjectBlog/static/skrypt.js
+++ _WEB_DEV_/ProjectBlog/static/skrypt.js
@@ -1,1 +1,23 @@
-console.log("siema!");
+
+function klik() {
+
+    var daneUsera = document.getElementById('daneUsera').value;
+    console.log(daneUsera);
+}
+
+function wyslijJsona(dane, callback) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // var json = JSON.parse(xhr.responseText);
+            // console.log("zwrotka: "+json.dane);
+            callback();
+        }
+    };
+
+    var data = JSON.stringify(dane);
+    xhr.send(data);
+}
