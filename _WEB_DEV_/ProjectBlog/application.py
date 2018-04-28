a59d172e22ea1cc83cd8139dc7c6649338112327
kfigon <kamil.figon@jitsolutions.pl>
2018-04-28 22:55:40 +0200

    blog apka in progress

--- _WEB_DEV_/ProjectBlog/application.py
+++ _WEB_DEV_/ProjectBlog/application.py
@@ -5,17 +5,38 @@ app = Flask(__name__)
 
 def czytajPosty():
     return [
-        {'tytul': 'Bylem wczoraj tutaj', 'tresc': 'bylo awesome!'},
-        {'tytul': 'Nowi avengersi', 'tresc': 'nie podobaja mi sie'},
-        {'tytul': 'taki sobie wpis', 'tresc': 'ale bez tresci...'}
+        {'id': 1, 'tytul': 'Bylem wczoraj tutaj', 'tresc': 'bylo awesome!'},
+        {'id': 2, 'tytul': 'Nowi avengersi', 'tresc': 'nie podobaja mi sie'},
+        {'id': 3, 'tytul': 'taki sobie wpis', 'tresc': 'ale bez tresci...'}
     ]
 
 @app.route('/')
 @app.route('/index')
 def dawajStart():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/posty')
+def posty():
+    return render_template('posty.html', posty=czytajPosty())
+
+@app.route('/post/<string:id>')
+def post(id):
     posty = czytajPosty()
-    return render_template('index.html',
-    posty=posty)
+    post = None
+    for p in posty:
+        if(p['id'] == int(id)):
+            post = p
+            break
+
+    return render_template('post.html', post = post)
+
+@app.route('/newpost', methods=['GET', 'POST'])
+def nowy():
+    return 'ok'
 
 if __name__=='__main__':
     app.run(debug=True)
