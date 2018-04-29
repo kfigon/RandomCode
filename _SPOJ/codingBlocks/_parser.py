afe0f853cf2f60c3ec1c05079ad2442fd0b51920
kfigon <kamil.figon@jitsolutions.pl>
2018-04-29 23:38:26 +0200

    lucky sum

--- _SPOJ/codingBlocks/_parser.py
+++ _SPOJ/codingBlocks/_parser.py
@@ -1,7 +1,7 @@
 def parsujLinijke(linijka):
     frazy = linijka.split(' → ')
     logic = frazy[1].split('\t')[0]
-    return ('self.assert{}({})'.format(logic, frazy[0]))
+    return ('self.assertEqual({}, {})'.format(logic, frazy[0]))
     
 def parser(napis):
     out = []
@@ -19,39 +19,22 @@ def robPlik(nazwaPliku, nazwaTestu, napis):
             plik.write('    def test'+str(i)+'(self):\n')
             plik.write('        '+t+'\n')
         
-        plik.write("if _name__=='__main_':\n")
+        plik.write("if __name__=='__main__':\n")
         plik.write("    unittest.main()")
 
 
 
-data = '''make_bricks(3, 1, 8) → True	None	X	
-make_bricks(3, 1, 9) → False	None	X	
-make_bricks(3, 2, 10) → True	None	X	
-make_bricks(3, 2, 8) → True	None	X	
-make_bricks(3, 2, 9) → False	None	X	
-make_bricks(6, 1, 11) → True	None	X	
-make_bricks(6, 0, 11) → False	None	X	
-make_bricks(1, 4, 11) → True	None	X	
-make_bricks(0, 3, 10) → True	None	X	
-make_bricks(1, 4, 12) → False	None	X	
-make_bricks(3, 1, 7) → True	None	X	
-make_bricks(1, 1, 7) → False	None	X	
-make_bricks(2, 1, 7) → True	None	X	
-make_bricks(7, 1, 11) → True	None	X	
-make_bricks(7, 1, 8) → True	None	X	
-make_bricks(7, 1, 13) → False	None	X	
-make_bricks(43, 1, 46) → True	None	X	
-make_bricks(40, 1, 46) → False	None	X	
-make_bricks(40, 2, 47) → True	None	X	
-make_bricks(40, 2, 50) → True	None	X	
-make_bricks(40, 2, 52) → False	None	X	
-make_bricks(22, 2, 33) → False	None	X	
-make_bricks(0, 2, 10) → True	None	X	
-make_bricks(1000000, 1000, 1000100) → True	None	X	
-make_bricks(2, 1000000, 100003) → False	None	X	
-make_bricks(20, 0, 19) → True	None	X	
-make_bricks(20, 0, 21) → False	None	X	
-make_bricks(20, 4, 51) → False	None	X	
-make_bricks(20, 4, 39) → True	None'''
+data = '''lucky_sum(1, 2, 3) → 6	None	X	
+lucky_sum(1, 2, 13) → 3	None	X	
+lucky_sum(1, 13, 3) → 1	None	X	
+lucky_sum(1, 13, 13) → 1	None	X	
+lucky_sum(6, 5, 2) → 13	None	X	
+lucky_sum(13, 2, 3) → 0	None	X	
+lucky_sum(13, 2, 13) → 0	None	X	
+lucky_sum(13, 13, 2) → 0	None	X	
+lucky_sum(9, 4, 13) → 13	None	X	
+lucky_sum(8, 13, 2) → 8	None	X	
+lucky_sum(7, 2, 1) → 10	None	X	
+lucky_sum(3, 3, 13) → 6	None	X'''
 
-robPlik(r'C:\Users\Kamil\Desktop\skrypty\RandomCode\_SPOJ\Srednie\bricks\test.py', 'TestBricks', data)
+robPlik(r'C:\Users\Kamil\Desktop\skrypty\RandomCode\_SPOJ\codingBlocks\test.py', 'TestLuckySum', data)
