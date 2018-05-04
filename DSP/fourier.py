047741482bc2ea051515e03362c4b85568478f96
kfigon <kamil.figon@jitsolutions.pl>
2018-05-04 15:23:43 +0200

    fourier tweaks

--- DSP/fourier.py
+++ DSP/fourier.py
@@ -9,7 +9,7 @@ t = np.arange(0,30,step)
 y = np.cos(2*3.1415*f *t)
 
 
-plt.subplot(4,1,1)
+plt.subplot(3,2,1)
 plt.title('Sygnal <0;30) krok 0.1')
 plt.xlabel('time')
 plt.ylabel('cos(2pi*n)')
@@ -19,17 +19,22 @@ plt.plot(t,y)
 # DTFT jest dla okresowych
 yt = scipy.fftpack.fft(y)
 
-plt.subplot(4,1,2)
+plt.subplot(3,2,3)
 plt.title('Fourier Re')
 plt.stem(t, np.real(yt))
 
-plt.subplot(4,1,3)
+plt.subplot(3,2,4)
 plt.title('Fourier Imag')
 plt.stem(t, np.imag(yt))
 
-plt.subplot(4,1,4)
+plt.subplot(3,2,5)
 plt.title('Fourier abs')
 plt.stem(t, np.abs(yt))
+
+plt.subplot(3,2,6)
+plt.title('Fourier phas')
+plt.stem(t, np.angle(yt))
+
 
 plt.show()
 
@@ -44,5 +49,6 @@ pik cosinusa na probce '3', czyli 30 (indeks od 0, krok 0.1)
 dlugosc sygnalu to 300. czestotliwosc to 1. jakas faza sie pojawila, bo imag niezerowy (nie powinno tutaj byc nic?)
 
 w = numerProbkiZPikiem * 2pi/dlugoscSygnalu
+czestotliwosc: skoro max f to fs/2 i odpowiada ono N/2, dzielac jedno przez drugie dostajemy Fs/N <- rozdzielczosc wykresu
 label - skoro max cz. to Fs/2 i wychodzi w polowie fouriera (N/2), mozna wyskalowac wykres
 '''
