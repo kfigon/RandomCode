69fe1e76a4864222a65a76df1b1f5b6f93ebc814
kfigon <kamil.figon@jitsolutions.pl>
2018-05-07 12:18:06 +0200

    sql refactor

--- sql/moreFoo.py
+++ sql/moreFoo.py
@@ -1,26 +1,27 @@
 import sqlite3
 
-def createPies():
-    return 'CREATE TABLE Pies(imie varchar(30), wiek integer)'
-
-def populatePieses():
-    return [('Azor',12),('Fafik', 2),('Dziku',4),('Kurczak', 5)]
-
-def readAllPiesels():
-    return 'SELECT * FROM Pies'
-
 # with nie dziala!
-db= sqlite3.connect(':memory:')
+db = sqlite3.connect(':memory:')
+db.row_factory = sqlite3.Row
 
 cursor = db.cursor()
-cursor.execute(createPies())
-    
-piesely = populatePieses()
+cursor.execute('''CREATE TABLE Pies(
+    id INTEGER PRIMARY KEY, 
+    imie VARCHAR(30), 
+    wiek INTEGER)
+    ''')
+
+piesely = [('Azor',12),('Fafik', 2),('Dziku',4),('Kurczak', 5)]    
 cursor.executemany('INSERT INTO Pies(imie, wiek) VALUES(?,?)',
                    piesely)
 
-for i in cursor.execute(readAllPiesels()):
-    print(i)
+
+# for i in cursor.execute('SELECT * FROM Pies'):
+results = cursor.execute('SELECT * FROM Pies').fetchall()
+for i in results:
+    # print(i['imie'])
+    print(dict(i))
+
 
 db.close()
 
