7622893216ee05ae905f5fac5782e8fd79a67069
kfigon <kamil.figon@jitsolutions.pl>
2018-07-27 16:07:16 +0200

    aplikowanie sily

--- fizyka/grawitacja.py
+++ fizyka/grawitacja.py
@@ -5,19 +5,22 @@ class Pilka:
     def __init__(self):
         self.location = Wektor(50,0)
         self.velocity = Wektor(0,0)
-        self.acceleration = Wektor(0,0.3)
+        self.acceleration = Wektor(0,0)
 
     def update(self, w, h):
         loc = self.location
         if(loc.y >= h):
-            loc.y=0
+            self.location = Wektor(50,0)
             self.velocity = Wektor(0,0)
+            # self.acceleration = Wektor(0,0)
             return        
             # F = m*a
             # sila (wektor) zmienia szybkosc, szybkosc zmienia polozenie
         self.velocity.dodajWektor(self.acceleration)
         self.location.dodajWektor(self.velocity)
 
+    def zaaplikujSile(self, sila):
+        self.acceleration.dodajWektor(sila)
 
 class update:
     def __init__(self, cavas, pilka, w, h):
@@ -39,4 +42,9 @@ class update:
 o = Okno(50)
 c = o.canvas
 pilka = Pilka()
+
+grawitacja = Wektor(0,0.3)
+pilka.zaaplikujSile(grawitacja)
+
+
 o.start(update(c, pilka, o.WIDTH, o.HEIGHT))
