0c1f32415e4693b136c8ace742425444ac7fd068
Kamil <kamil.figon@gmail.com>
2020-04-17 19:17:47 +0200

    tic tac toe

--- _Games/kolkokrzyzyk-typed/app.py
+++ _Games/kolkokrzyzyk-typed/app.py
@@ -1,5 +1,71 @@
-def sayHello(name: str) -> str:
-    return 'hello {}!'.format(name)
+from typing import List, Callable
+from enum import Enum
+import logging
 
-def sayAgain(name: str) -> str:
-    return f'hello {name}!'
+class Field(Enum):
+    EMPTY = 0
+    X = 1
+    O = 2
+
+class GameResult(Enum):
+    PENDING = 0
+    X_WIN = 1
+    O_WIN = 2
+    TIE = 3
+
+class TicTacToe:
+
+    
+    def __init__(self):
+        self.gameArea :List[Field] = [Field.EMPTY for _ in range(9)]
+        self.currentPlayer: Field = Field.X
+
+    # 0 1 2
+    # 3 4 5
+    # 6 7 8
+    def getStatus(self) -> GameResult:
+        if self.__doesPlayerWon(Field.X):
+            return GameResult.X_WIN
+        elif self.__doesPlayerWon(Field.O):
+            return GameResult.O_WIN
+        elif self.__anyEmptyPlace():
+            return GameResult.PENDING
+        return GameResult.TIE
+
+    def __anyEmptyPlace(self) -> bool:
+        for f in self.gameArea:
+            if f == Field.EMPTY:
+                return True
+        return False
+
+    def __doesPlayerWon(self, player :Field) -> bool:
+        g = self.gameArea
+        
+        isHorisontalWin :Callable[[int], bool] = lambda row: g[3*row] == player and g[3*row+1] == player and g[3*row+2] == player
+        isVerticalWin :Callable[[int], bool] = lambda col: g[col] == player and g[col+3] == player and g[col+6] == player
+        
+        for i in range(3):
+            if isHorisontalWin(i) or isVerticalWin(i):
+                return True
+
+        if (g[0] == player and g[4] == player and g[8] == player) or (g[2] == player and g[4] == player and g[6] == player):
+            return True
+        
+        return False
+
+    def move(self, position: int) -> None:
+        if position < 0 or position >= len(self.gameArea):
+            raise ValueError(f'Invalid move to {position}')
+
+        elif self.gameArea[position] != Field.EMPTY:
+            raise ValueError(f'Invalid move to {position}, field is taken by {self.gameArea[position]}')
+
+        logging.info(f'moving {self.currentPlayer} to {position}')
+        self.gameArea[position] = self.currentPlayer
+        self.__changePlayer()
+               
+    def __changePlayer(self) -> None:
+        if self.currentPlayer == Field.X:
+            self.currentPlayer = Field.O
+        else:
+            self.currentPlayer = Field.X
--- _Games/kolkokrzyzyk-typed/test/testApp.py
+++ _Games/kolkokrzyzyk-typed/test/testApp.py
@@ -1,12 +1,54 @@
 import unittest
-from app import sayHello, sayAgain
+import logging
+from app import TicTacToe, Field, GameResult
 
 class TestFoo(unittest.TestCase):
-    def test0(self):
-        self.assertEqual('hello andy!', sayHello('andy'))
+    def setUp(self):
+        logging.basicConfig(level=logging.INFO)
+        self.g = TicTacToe()
+
+    def testStart(self):
+        self.assertEqual([Field.EMPTY]*9, self.g.gameArea)
+        self.assertEqual(Field.X, self.g.currentPlayer)
+        self.assertEqual(GameResult.PENDING, self.g.getStatus())
+
+    def testIsOver2(self):
+        self.g.move(0)
+        self.assertEqual(Field.O, self.g.currentPlayer)
+        self.assertEqual(GameResult.PENDING, self.g.getStatus())
     
-    def test1(self):
-        self.assertEqual('hello andy!', sayAgain('andy'))
+    def testMoveToInvalid(self):
+        case = [-2, -1, 9, 10, 11]
+        for x in case:
+            with self.subTest(name=f'when {x} then throw'):
+                self.assertRaises(ValueError, self.g.move, x)
+
+    def testGameResult(self):
+        case = [
+            {'result': GameResult.PENDING, 'steps':[0,1,3]},
+            {'result': GameResult.PENDING, 'steps':[4,3,1,7]},
+            {'result': GameResult.PENDING, 'steps':[0,4,2,3,6]},
+
+            {'result': GameResult.TIE, 'steps':[4,0,6,2,1,7,3,5,8]},
+
+            {'result': GameResult.X_WIN, 'steps':[0,4,1,3,2 ]},
+            {'result': GameResult.X_WIN, 'steps':[3,1,4,2,5 ]},
+            {'result': GameResult.X_WIN, 'steps':[6,1,7,2,8 ]},
+
+            {'result': GameResult.X_WIN, 'steps':[0,1,3,2,6 ]},
+            {'result': GameResult.X_WIN, 'steps':[1,0,4,2,7 ]},
+            {'result': GameResult.X_WIN, 'steps':[2,1,5,4,8 ]},
+
+            {'result': GameResult.X_WIN, 'steps':[0,1,4,2,8 ]},
+            {'result': GameResult.X_WIN, 'steps':[2,1,4,7,6 ]},
+        ]
+        for x in case:
+            self.g = TicTacToe()
+            with self.subTest(name=f'steps: {x["steps"]}'):
+                for s in x['steps']:
+                    self.g.move(s)
+                self.assertEqual(x['result'], self.g.getStatus())
+
 
 if __name__=='__main__':
     unittest.main()
