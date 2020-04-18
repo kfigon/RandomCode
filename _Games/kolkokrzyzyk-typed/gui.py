1cd1b5b255bbea6eee325056b086c9237e139a81
kfigon <kamil.figon@gmail.com>
2020-04-18 15:54:06 +0200

    basic gui

--- _Games/kolkokrzyzyk-typed/gui.py
+++ _Games/kolkokrzyzyk-typed/gui.py
@@ -4,16 +4,23 @@ class Gui:
     def __init__(self):
         self.root = tk.Tk()
         self.plotno = tk.Canvas(self.root, width=300, height=300)
-        tk.Button(self.root, text='czysc', command=lambda: self.plotno.delete('all')).pack()
+        tk.Button(self.root, text='czysc', command=self.czysc).pack()
+        tk.Button(self.root, text='rysuj', command=self.draw).pack()
+        tk.Button(self.root, text='zamknij', command=self.end).pack()
+
+        self.plotno.pack()
+
+    def czysc(self):
+        self.plotno.delete('all')
 
     def draw(self) -> None:
         self.plotno.create_oval(10,20, 40, 40, fill='blue')
         self.plotno.create_line(0, 40, 300, 340, width=2, fill='black')
 
     def start(self) -> None:
-        self.plotno.pack()
         self.root.mainloop()
+    
+    def end(self) -> None:
+        self.root.destroy()
 
-g = Gui()
-g.draw()
-g.start()
+Gui().start()
