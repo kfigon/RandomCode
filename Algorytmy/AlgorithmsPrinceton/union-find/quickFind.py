c6a34d1d5851e840c9d3fa80e255477a442e2df2
Kamil <kamil.figon@gmail.com>
2020-09-03 21:53:06 +0200

    quick find implemented

--- Algorytmy/AlgorithmsPrinceton/union-find/quickFind.py
+++ Algorytmy/AlgorithmsPrinceton/union-find/quickFind.py
@@ -5,10 +5,37 @@ class QuickFind:
         self.tab : List[int] = [i for i in range(rozmiar)]
 
     def connected(self, p, q) -> bool:
-        return False
+        return self.tab[p] == self.tab[q]
 
     def union(self, p,q):
-        pass
+        if self.connected(p, q):
+            return
+
+        finalIdx = self.tab[q]
+        searchingIdx = self.tab[p]
+        for i in range(len(self.tab)):
+            if self.tab[i] == searchingIdx:
+                self.tab[i] = finalIdx
 
     def getNumberOfConnectedComponents(self) -> int:
-        pass
+        uniqueIdxs : List[int] = []
+        for i in self.tab:
+            if i not in uniqueIdxs:
+                uniqueIdxs.append(i)
+
+        return len(uniqueIdxs)
+
+
+if __name__ == "__main__":
+    uf = QuickFind(10)
+    uf.union(4,3)
+    uf.union(3,8)
+    uf.union(6,5)
+    uf.union(9,4)
+    uf.union(2,1)
+    uf.union(8,9)
+    uf.union(5,0)
+    uf.union(7,2)
+    uf.union(6,1)
+
+    print(uf.tab)
--- Algorytmy/AlgorithmsPrinceton/union-find/testQuickFind.py
+++ Algorytmy/AlgorithmsPrinceton/union-find/testQuickFind.py
@@ -37,7 +37,44 @@ class Tests(unittest.TestCase):
             with self.subTest(name=nazwa):
                 res = uf.connected(x[0],x[1])
                 self.assertEqual(expectedResult, res)
+        
+        self.assertEqual(3, uf.getNumberOfConnectedComponents())
 
+    def testUnions(self):
+        uf = QuickFind(10)
+        uf.union(4,3)
+        uf.union(3,8)
+        uf.union(6,5)
+        uf.union(9,4)
+        uf.union(2,1)
+        uf.union(8,9)
+        uf.union(5,0)
+        uf.union(7,2)
+        uf.union(6,1)
 
+        self.assertTrue(uf.connected(0,5))
+        self.assertTrue(uf.connected(0,1))
+        self.assertTrue(uf.connected(0,6))
+        self.assertTrue(uf.connected(0,7))
+        self.assertTrue(uf.connected(0,2))
+        
+        self.assertTrue(uf.connected(3,9))
+        self.assertTrue(uf.connected(3,8))
+        self.assertTrue(uf.connected(4,8))
+
+        self.assertFalse(uf.connected(3,0))
+
+        self.assertEqual(2, uf.getNumberOfConnectedComponents())
+    
+
+    def test2(self):
+        uf = QuickFind(5)
+        uf.tab=[0,1,1,8,8,0,0,1,8,8]
+        self.assertFalse(uf.connected(6,1))
+        self.assertFalse(uf.connected(1,6))
+        uf.union(6,1)
+        self.assertTrue(uf.connected(6,1))
+        self.assertTrue(uf.connected(1,6))
+        
 if __name__ == "__main__":
     unittest.main()
