112ec6194ed8152ce7d074944e36b301a8e92d16
Kamil <kamil.figon@gmail.com>
2020-09-06 20:16:47 +0200

    some test improvement

--- Algorytmy/AlgorithmsPrinceton/binarySearch/testBinarySearch.py
+++ Algorytmy/AlgorithmsPrinceton/binarySearch/testBinarySearch.py
@@ -48,6 +48,17 @@ class TestFoo(unittest.TestCase):
             with self.subTest(k):
                 self.assertEqual(-1, bs.binarySearch(k))  
 
+    def testStress(self):
+        sizes = [4,5,6,7,8,20,21,22,23,30,31]
+        for size in sizes:
+            for _ in range(100):
+                ar = [random.randint(-5, 20) for _ in range(size)]
+                ar.sort()
+                bs = BinarySearch(ar)
+                randomElement = ar[random.randint(0, len(ar)-1)]
+                foundId = bs.binarySearch(randomElement)
+                self.assertEqual(randomElement, ar[foundId])
+
 class TestyPrzedzialu(unittest.TestCase):
     def testMiddle(self):
         self.assertEqual(2, Przedzial(0, 5).getMiddleIdx())
@@ -57,7 +68,7 @@ class TestyPrzedzialu(unittest.TestCase):
         self.assertEqual(5, Przedzial(5, 5).getMiddleIdx())
 
 if __name__ == "__main__":
-    # logging.basicConfig(level=logging.WARNING)
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.WARNING)
+    # logging.basicConfig(level=logging.INFO)
 
     unittest.main()
