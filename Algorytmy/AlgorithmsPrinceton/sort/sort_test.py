16baffa0c0c1dbff059ce3d7514d33bb9a41ef40
Kamil <kamil.figon@gmail.com>
2020-09-13 21:31:56 +0200

    selection/insertion sorts

---
+++ Algorytmy/AlgorithmsPrinceton/sort/sort_test.py
@@ -0,0 +1,64 @@
+import unittest
+from typing import List
+
+class BaseSort:
+    def sort(self, tab: List[int]) -> List[int]:
+        raise Exception('not implemented')
+    
+class SelectionSort(BaseSort):
+    def sort(self, tab: List[int]) -> List[int]:
+        # quadratic performance - always!
+        dl = len(tab)
+        for i in range(dl):
+            smallestIdx = i
+            for j in range(smallestIdx+1, dl):
+                if tab[j] < tab[smallestIdx]:
+                    smallestIdx = j
+
+            tmp = tab[i]
+            tab[i] = tab[smallestIdx]
+            tab[smallestIdx] = tmp
+        return tab
+
+class InsertionSort(BaseSort):
+    def sort(self, tab: List[int]) -> List[int]:
+        for i in range(len(tab)):
+            j = i
+            prev = j-1
+            while (j > 0 and tab[j] < tab[prev]):
+                tab[j], tab[prev] = tab[prev], tab[j]
+                j -= 1
+                prev-=1
+
+            # for j in range(i, 0, -1):
+                # prev = j-1
+                # if tab[j] < tab[prev]:
+                    # tab[j], tab[prev] = tab[prev], tab[j]
+                # else:
+                    # break
+
+        return tab
+
+class SortTest(unittest.TestCase):
+
+    def doSort(self, bs: BaseSort):
+        for ar in self.getTestArrays():
+            ar, exp = ar
+            res: List[int] = bs.sort(ar)
+            self.assertEqual(exp, res)
+
+    def getTestArrays(self):
+        return[
+            ([7, 10, 5,3,8,4,2,9,6], [2,3,4,5,6,7,8,9,10]),
+            ([20,7,2, 10, 5,3,-1,8,4,3,2,9,6,20], [-1,2,2,3,3,4,5,6,7,8,9,10,20,20])
+        ]
+
+    def testSelection(self):
+        self.doSort(SelectionSort())
+    
+    def testInsertionSort(self):
+        self.doSort(InsertionSort())
+
+
+if __name__ == "__main__":
+    unittest.main()
