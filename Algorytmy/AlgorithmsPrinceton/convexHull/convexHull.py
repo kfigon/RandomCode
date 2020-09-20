e8c647f52f6a3f4ec22e13e40abaf15d95038fa9
Kamil <kamil.figon@gmail.com>
2020-09-20 22:49:57 +0200

    convex hull done

--- Algorytmy/AlgorithmsPrinceton/convexHull/convexHull.py
+++ Algorytmy/AlgorithmsPrinceton/convexHull/convexHull.py
@@ -11,23 +11,44 @@ class Point:
     x: int
     y: int
 
-def angle(a: Point, b: Point) -> float:
-    return math.atan2((b.y-a.y), (b.x-a.x))
+    def __repr__(self):
+        return f'({self.x},{self.y})'
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+def sortingFunction(lowest: Point, a: Point) -> float:
+    return math.atan2((a.y-lowest.y), (a.x-lowest.x))
 
 # is c to the left of ray a->b
 # a->b->c
 def isCounterClockwise(a: Point, b: Point,c: Point) -> bool:
-    res = (b.x - a.x)*(c.y-a.y) - (b.y-a.y)*(c.x-a.x)
+    res: int = (b.x - a.x)*(c.y-a.y) - (b.y-a.y)*(c.x-a.x)
     # res < 0 - clockwise, >0 counter, == 0 - collinear
     return res > 0
 
+# https://algorithmtutor.com/Computational-Geometry/Convex-Hull-Algorithms-Graham-Scan/
 class GrahamScan:
     def __init__(self, points: List[Point]):
         self.points = points
 
+    def findSmallest(self) -> Point:
+        smallest = None
+        for i in self.points:
+            if smallest is None:
+                smallest = i
+            elif(i.y < smallest.y):
+                smallest = i
+            elif (i.y == smallest.y and i.x < smallest.x):
+                smallest = i
+        return smallest
+
     def generate(self) -> List[Point]:
-        lowest = min(self.points, key=attrgetter('y'))
-        self.points.sort(key=lambda p: angle(p, lowest))
+        if len(self.points) <= 3:
+            return self.points
+
+        lowest = self.findSmallest()
+        self.points.sort(key=lambda p: sortingFunction(lowest, p))
+
         hull: List[Point] = [lowest, self.points[0]]
 
         for i in range(2, len(self.points)):
@@ -36,22 +57,27 @@ class GrahamScan:
             lastEntry = hull[-1]
             while not isCounterClockwise(lastEntry, top, currentPoint):
                 top = hull.pop()
+                lastEntry = hull[-1]
+
             hull.append(top)
             hull.append(currentPoint)
         
         return hull
 
 def parseInput(string : str) -> List[Point]:
-    splittedLines = map(lambda line: line.split(' '), string.splitlines())
+    splittedLines = map(lambda line: line.split(' '), string.strip().splitlines())
     return list(map(lambda vals: Point(int(vals[0]), int(vals[1])), splittedLines))
 
 if __name__ == "__main__":        
-    inputString = '''1 1
-4 4
-5 3
-6 4'''
+    inputString = '''7 0
+1 4
+3 1 
+3 3 
+5 2
+5 5
+0 0
+9 6'''
 
     points: List[Point] = parseInput(inputString)
     gs = GrahamScan(points)
-    #  zle!
     print(gs.generate())
