9d4c8c1b91bee7bc485f9cbec74e58bea671dabf
Kamil <kamil.figon@gmail.com>
2020-10-11 15:10:09 +0200

    some algorithms

---
+++ Algorytmy/udemyAlgorithms/popularProblems/charCounter.py
@@ -0,0 +1,57 @@
+from typing import Dict, List
+import random
+import string
+import time
+
+def getRandom(length: int) -> str:
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+def countCharsSimple(input: str) -> Dict[str, int]:
+    out : Dict[str, int] = {}
+    for i in input:
+        out[i] = 1 if i not in out else out[i] + 1
+    return out
+
+# not so pro - sorting introduces more time
+def countCharsPro(input: str) -> Dict[str, int]:
+    sort : List[str] = sorted(input)
+    out : Dict[str, int] = {}
+    counter = 0
+    i = 1
+    prevChar=''
+
+    while(i<len(sort)):
+        prevChar = sort[i-1]
+        char=sort[i]
+        counter += 1
+        if prevChar != char:
+            out[prevChar] = counter
+            counter = 0
+        i+=1
+            
+    out[sort[-1]] = counter+1
+    return out
+
+assert countCharsSimple('missisipi') == countCharsPro('missisipi')
+assert countCharsSimple('falanga') == countCharsPro('falanga')
+assert countCharsSimple('johndoe') == countCharsPro('johndoe')
+assert countCharsSimple('johndoee') == countCharsPro('johndoee')
+
+print('generating')
+words = [getRandom(1000) for i in range(10000)]
+    
+print('tests and timing')
+results = {'simple':0, 'pro':0}
+for i in words:
+    start = time.time_ns()
+    sResult = countCharsSimple(i)
+    results['simple'] += time.time_ns()-start
+
+    start2 = time.time_ns()
+    pResult = countCharsPro(i)
+    results['pro'] += time.time_ns()-start2
+    assert sResult == pResult
+
+print(f"simple version: {results['simple']/len(words)} ms")
+print(f"pro version: {results['pro']/len(words)} ms")
---
+++ Algorytmy/udemyAlgorithms/popularProblems/frequencyCounter.py
@@ -0,0 +1,73 @@
+from typing import List, Dict
+
+# return true if every value in the array has it's corresponding value
+# squared in the second array
+# frequency of values must be the same
+
+def howMany(val: int, b: List[int]) -> int:
+    return len(list(filter(lambda x: x == val, b)))
+
+def sameBrute(a: List[int], b: List[int]) -> bool:
+    for i in range(len(a)):
+        if howMany(a[i], a) != howMany(a[i]**2, b):
+            return False
+    return True
+
+# troche lepsze - mniej petli
+def sameBrute2(a: List[int], b: List[int]) -> bool:
+    for i in range(len(a)):
+        if (a[i]**2) not in b:
+            return False
+        b.remove(a[i]**2)
+    return True
+
+def countFreq(a: List[int]) -> Dict[int, int]:
+    out : Dict[int, int] = {}
+    for i in a:
+        out[i] = 1 if i not in out else out[i] + 1
+    return out
+
+# 2 petle sa lepsze niz nested!
+def same(a: List[int], b: List[int]) -> bool:
+    if len(a) != len(b):
+        return False
+    freqA = countFreq(a)
+    freqB = countFreq(b)
+
+    for key in freqA:
+        # search for key is fast!
+        if key**2 not in freqB:
+            return False
+
+        vA = freqA[key]
+        vB = freqB[key**2]
+        if vA != vB:
+            return False
+    return True
+        
+
+assert howMany(1, [1,2,3]) == 1
+assert howMany(2, [1,2,3]) == 1
+assert howMany(1, [1,2,1]) == 2
+
+
+assert sameBrute([1,2,3], [4,1,9]) == True
+assert sameBrute([1,2,3], [1,9]) == False
+assert sameBrute([1,2,1], [4,4,1]) == False
+assert sameBrute([1,2,1], [4,1,1]) == True
+assert sameBrute([1,2,3,2], [9,1,4,4]) == True
+
+assert sameBrute2([1,2,3], [4,1,9]) == True
+assert sameBrute2([1,2,3], [1,9]) == False
+assert sameBrute2([1,2,1], [4,4,1]) == False
+assert sameBrute2([1,2,1], [4,1,1]) == True
+assert sameBrute2([1,2,3,2], [9,1,4,4]) == True
+
+assert same([1,2,3], [4,1,9]) == True
+assert same([1,2,3], [1,9]) == False
+assert same([1,2,1], [4,4,1]) == False
+assert same([1,2,1], [4,1,1]) == True
+assert same([1,2,3,2], [9,1,4,4]) == True
+
+
+
