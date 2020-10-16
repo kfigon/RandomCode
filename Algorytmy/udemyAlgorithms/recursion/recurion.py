53ff8f6db62cb250f461f250cb155fb3e71d6263
Kamil <kamil.figon@gmail.com>
2020-10-16 19:22:46 +0200

    basic recursion algorithms

--- Algorytmy/udemyAlgorithms/recursion/recurion.py
+++ Algorytmy/udemyAlgorithms/recursion/recurion.py
@@ -28,6 +28,8 @@ def sumRange(num: int) -> int:
         return 1
     return num + sumRange(num-1)
 
+assert sumRange(6) == 21
+assert sumRange(10) == 55
 assert sumRange(5) == 15
 assert sumRange(4) == 10
 assert sumRange(3) == 6
@@ -35,10 +37,11 @@ assert sumRange(30) == 465
 
 
 def factorial(num: int) -> int:
-    if num == 1:
+    if num <= 1:
         return 1
     return num * factorial(num-1)
 
+assert factorial(0) == 1
 assert factorial(1) == 1
 assert factorial(2) == 2
 assert factorial(3) == 6
@@ -62,7 +65,7 @@ def collectOdd(tab: List[int]) -> List[int]:
 assert collectOdd([1,2,3,4,5,6]) == [1,3,5]
 
 def collectOddPure(tab: List[int]) -> List[int]:
-    newArray = []
+    newArray: List[int] = []
     if len(tab) == 0:
         return newArray
     if tab[0] % 2 == 1:
@@ -71,4 +74,35 @@ def collectOddPure(tab: List[int]) -> List[int]:
     # concat
     return newArray + collectOddPure(tab[1:])
 
-print(collectOddPure([1,2,3,4,5,6,7]))
+assert collectOddPure([1,2,3,4,5,6,7]) == [1,3,5,7]
+
+def pow(a: int, b: int) -> int:
+    assert a >= 0 and b >=0
+    
+    if b == 0:
+        return 1
+    return a * pow(a,b-1)
+
+for a in range(10):
+    for b in range(10):
+        assert  pow(a,b) == a**b
+
+def productOfArray(tab: List[int]) -> int:
+    if len(tab) == 0:
+        return 1
+    return tab[0] * productOfArray(tab[1:])
+
+assert productOfArray([1,2,3]) == 6
+assert productOfArray([1,2,3, 10]) == 60
+
+def fib(x: int) -> int:
+    if x <= 0:
+        return 0
+    elif x == 1:
+        return 1
+    return fib(x-1) + fib(x-2)
+
+assert fib(4) == 3
+assert fib(10) == 55
+assert fib(28) == 317811
+assert fib(35) == 9227465
