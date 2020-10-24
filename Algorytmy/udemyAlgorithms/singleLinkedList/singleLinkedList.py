c978e3ade7fa99851a3420a76a43d024fc34793e
kfigon <kamil.figon@gmail.com>
2020-10-24 19:18:53 +0200

    linked list push pop done

--- Algorytmy/udemyAlgorithms/singleLinkedList/singleLinkedList.py
+++ Algorytmy/udemyAlgorithms/singleLinkedList/singleLinkedList.py
@@ -1,2 +1,47 @@
-class SingleLinkedList:
-    pass
+from typing import TypeVar, Generic, Optional, List
+T = TypeVar('T')
+
+class Node(Generic[T]):
+    def __init__(self, val: T):
+        self.val = val
+        self.next: Optional[Node[T]] = None
+
+class SingleLinkedList(Generic[T]):
+    def __init__(self):
+        self.head: Optional[Node[T]] = None
+        self.tail: Optional[Node[T]] = None
+        self.length: int = 0
+    
+    def push(self, val: T):
+        newNode = Node[T](val)
+        self.length+=1
+        if self.head is None:
+            self.head = newNode
+        else:
+            assert self.tail, 'this shouldnt happen!' # just to satisfy mypy...
+            self.tail.next = newNode
+                
+        self.tail = newNode
+    
+    def pop(self) -> Optional[T]:
+        if self.head is None:
+            return None
+        elif self.head.next is None:
+            self.length -=1
+            v = self.head.val
+            self.head = None
+            self.tail = None
+            return v
+
+        newLast: Node = self.head
+        while newLast.next != self.tail and newLast.next is not None:
+            newLast = newLast.next
+        
+        assert self.tail # mypy
+        v = self.tail.val
+        
+        newLast.next = None
+        self.tail = newLast
+        
+        self.length -= 1
+        return v
--- Algorytmy/udemyAlgorithms/singleLinkedList/testSingleLinkedList.py
+++ Algorytmy/udemyAlgorithms/singleLinkedList/testSingleLinkedList.py
@@ -2,8 +2,66 @@ import unittest
 from singleLinkedList import SingleLinkedList
 
 class Test(unittest.TestCase):
-    def test0(self):
-        self.assertEqual(1,0)
+    def testPush(self):
+        s = SingleLinkedList[int]()
+        s.push(1)
+        s.push(2)
+        s.push(3)
+        self.assertEqual(s.length, 3)
+        self.assertEqual(s.head.val, 1)
+        self.assertEqual(s.head.next.val, 2)
+        self.assertEqual(s.head.next.next.val, 3)
+        self.assertIsNone(s.head.next.next.next)
+        self.assertEqual(s.tail.val, 3)
+        self.assertIsNone(s.tail.next)
+
+    def testPop3(self):
+        s = SingleLinkedList[int]()
+        s.push(1)
+        s.push(2)
+        s.push(3)
+
+        v = s.pop()
+
+        self.assertEqual(v, 3)
+        self.assertEqual(s.length, 2)
+
+        self.assertEqual(s.head.val, 1)
+        self.assertEqual(s.head.next.val, 2)
+        self.assertIsNone(s.head.next.next)
+        self.assertIsNone(s.tail.next)
+        self.assertEqual(s.tail.val, 2)
+
+    def testPop2(self):
+        s = SingleLinkedList[int]()
+        s.push(1)
+        s.push(2)
+
+        v = s.pop()
+
+        self.assertEqual(v, 2)
+        self.assertEqual(s.length, 1)
+
+        self.assertEqual(s.head.val, 1)
+        self.assertEqual(s.tail.val, 1)
+
+        self.assertIsNone(s.head.next)
+        self.assertIsNone(s.tail.next)
+
+    def testSinglePop(self):
+        s = SingleLinkedList[int]()
+        s.push(1)
+        self.assertEqual(s.pop(), 1)
+        self.assertIsNone(s.head)
+        self.assertIsNone(s.tail)
+        self.assertEqual(s.length, 0)
+        
+    def testEmptyPop(self):
+        s = SingleLinkedList[int]()
+        self.assertIsNone(s.pop())
+        self.assertIsNone(s.head)
+        self.assertIsNone(s.tail)
+        self.assertEqual(s.length, 0)
 
 if __name__ == "__main__":
     unittest.main()
