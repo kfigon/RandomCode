dd5bb42e807a35cef0ec5175437e633c9c1695cc
kfigon <kamil.figon@gmail.com>
2020-10-24 15:20:12 +0200

    template for singleLinkedList added

---
+++ Algorytmy/udemyAlgorithms/singleLinkedList/testSingleLinkedList.py
@@ -0,0 +1,9 @@
+import unittest
+from singleLinkedList import SingleLinkedList
+
+class Test(unittest.TestCase):
+    def test0(self):
+        self.assertEqual(1,0)
+
+if __name__ == "__main__":
+    unittest.main()
