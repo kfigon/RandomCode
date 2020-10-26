2a40c1d8ce6ca88ce170b8cee204e895abc46b03
kfigon <kamil.figon@gmail.com>
2020-10-26 20:12:57 +0100

    recursive insert

--- Algorytmy/udemyAlgorithms/binaryTree/binTree.py
+++ Algorytmy/udemyAlgorithms/binaryTree/binTree.py
@@ -31,7 +31,24 @@ class BinarySearchTree:
                 else:
                     ptr.left = newNode
                     return
+    
+    def insertRec(self, v:int):
+        newNode = Node(v)
+        def go(ptr: Node) -> Node:
+            if newNode.val > ptr.val:
+                return go(ptr.right) if ptr.right else ptr
+            else:
+                return go(ptr.left) if ptr.left else ptr
 
+        if not self.root:
+            self.root = newNode
+            return
+                    
+        ptr = go(self.root)
+        if newNode.val > ptr.val:
+            ptr.right = newNode
+        else:
+            ptr.left = newNode
 
     def find(self) -> Optional[int]:
         pass
--- Algorytmy/udemyAlgorithms/binaryTree/testBst.py
+++ Algorytmy/udemyAlgorithms/binaryTree/testBst.py
@@ -40,5 +40,38 @@ class TestBst(unittest.TestCase):
         self.assertIsNone(self.bst.root.right.left.left)
         self.assertIsNone(self.bst.root.right.left.left)
 
+    def testInsertRec(self):
+        self.bst.insertRec(10)
+        self.bst.insertRec(6)
+        self.bst.insertRec(15)
+        self.bst.insertRec(3)
+        self.bst.insertRec(8)
+        self.bst.insertRec(20)
+        self.bst.insertRec(13)
+
+        self.assertEqual(self.bst.root.val, 10)
+
+        self.assertEqual(self.bst.root.left.val, 6)
+        self.assertEqual(self.bst.root.right.val, 15)
+
+        self.assertEqual(self.bst.root.left.left.val, 3)
+        self.assertEqual(self.bst.root.left.right.val, 8)
+        
+        self.assertEqual(self.bst.root.right.left.val, 13)
+        self.assertEqual(self.bst.root.right.right.val, 20)
+
+        self.assertIsNone(self.bst.root.left.left.left)
+        self.assertIsNone(self.bst.root.left.left.right)
+        
+        self.assertIsNone(self.bst.root.left.right.left)
+        self.assertIsNone(self.bst.root.left.right.right)
+        
+        self.assertIsNone(self.bst.root.right.right.right)
+        self.assertIsNone(self.bst.root.right.right.left)
+        
+        self.assertIsNone(self.bst.root.right.left.left)
+        self.assertIsNone(self.bst.root.right.left.left)
+
+
 if __name__ == "__main__":
     unittest.main()
