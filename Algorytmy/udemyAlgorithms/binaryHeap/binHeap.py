6f23c744f46b2139bace8553b1769828df3ff41a
kfigon <kamil.figon@gmail.com>
2020-10-27 22:30:33 +0100

    binary heap using array - idx model

--- Algorytmy/udemyAlgorithms/binaryHeap/binHeap.py
+++ Algorytmy/udemyAlgorithms/binaryHeap/binHeap.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Tuple, Callable
 
 # BinaryHeap is kind of binarySearchTree (2 nodes)
 # maxBinaryHeap - parent is always larger than children nodes
@@ -29,3 +29,39 @@ class BinaryHeap:
             ptr = ptr.left if v < ptr.val else ptr.right
 
         return False
+
+
+# get child:
+# x * 2 + 1 
+# x * 2 + 2
+
+# get parent:
+# (n-1)//2
+
+# 0 -> 1,2
+# 1 -> 3,4
+# 2 -> 5,6
+# 3 -> 7,8
+# 4 -> 9,10
+# 5 -> 11,12
+class BinaryHeapArray:
+    def __init__(self):
+        self.tab: List[int] = []
+
+    def getChild(self, idx: int) -> Tuple[Optional[int], Optional[int]]:
+        assert idx < len(self.tab) and idx >= 0, f'invalid idx: {idx}, len: {len(self.tab)}'
+        
+        adjust: Callable[[int],Optional[int]] = lambda x: x if x < len(self.tab) else None
+        left = 2*idx + 1
+        right = 2*idx + 2        
+        return adjust(left), adjust(right)
+
+    def getParent(self, idx: int) -> Optional[int]:
+        assert idx < len(self.tab) and idx >= 0, f'invalid idx: {idx}, len: {len(self.tab)}'
+
+        parentIdx = (idx-1)//2
+        if parentIdx >= 0 and parentIdx < len(self.tab):
+            return parentIdx
+        return None
+
+    
