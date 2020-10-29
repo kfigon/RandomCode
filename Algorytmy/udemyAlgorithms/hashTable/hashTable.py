b5bdaec86a539c20cf85322490043c7f37428179
kfigon <kamil.figon@gmail.com>
2020-10-29 22:23:57 +0100

    hash tables done

---
+++ Algorytmy/udemyAlgorithms/hashTable/hashTable.py
@@ -0,0 +1,60 @@
+from typing import Optional, List, Tuple
+
+# hash table == hash map == dictionary
+
+# - to store key-value pairs
+# - elements are not ordered
+# - turbo fast for find/remove/add - very commonly used
+
+# inside regular array, but we change string to number and store
+# change string to number - hash
+# map 'pink' -> 0
+class HashTable:
+    def __init__(self, arrayLen = 19):
+        self.tab: List[List[Tuple[str, str]]] = [[] for _ in range(arrayLen)] # it's good to have the length as prime number to reduce collisions
+
+# collision handling approaches
+# separate chaining - store elements in the same position by using linked list or array
+# linear probing - find next empty slot when collision
+
+# here separate chaining:
+    def put(self, key: str, val: str):
+        hashVal = myBetterHashFun(key, len(self.tab))
+        toAdd = (key, val)
+        for el in self.tab[hashVal]:
+            if el[0] == key:
+                raise Exception(f'Duplicated key {key}')
+            
+        self.tab[hashVal].append(toAdd)
+            
+    def get(self, key: str) -> str:
+        hashVal = myBetterHashFun(key, len(self.tab))
+        candidate = self.tab[hashVal]
+        for i in candidate:
+            if i[0] == key:
+                return i[1]
+        raise Exception(f'Key error - {key} does not exist')
+
+
+# hash can't be worked backwards
+# hashes can repeat, there can be collisions
+# deterministic - hashes generate the same value for the same data consistently
+# fast (e.g. constant time)
+# uniform distribution (evenly distributed)
+
+# key and length of array, so hash is between <0;len)
+def myHashFun(key: str, length: int) -> int:
+    return sum(map(lambda c: ord(c)-ord('a')+1, key)) % length
+    # this has flaws, it's not constant time, could be more random, hashes only strings
+
+# using min() we limit loop
+# prime number makes distribution more random and spread, reduces collisions
+def myBetterHashFun(key: str, length: int) -> int:
+    v = 0
+    foo = min(len(key), 100)
+    weirdPrimeNumber = 31
+    for c in range(foo):
+        codeVal = ord(key[c])-96
+        v = (v*weirdPrimeNumber+codeVal) % length
+    # print(f'{key} -> {v}')
+    return v
---
+++ Algorytmy/udemyAlgorithms/hashTable/testHashTable.py
@@ -0,0 +1,55 @@
+import unittest
+from hashTable import HashTable, myHashFun, myBetterHashFun
+
+class TestHashTable(unittest.TestCase):
+    def test(self):
+        h = HashTable()
+        h.put('pink' ,'123')
+        h.put('foo' ,'bar')
+        self.assertEqual(h.get('pink'), '123')
+        self.assertEqual(h.get('foo'), 'bar')
+        self.assertRaises(Exception, h.get, 'cyan')
+        self.assertRaises(Exception, h.put, 'pink')
+
+        self.assertEqual(h.tab[15][0][0], 'pink')
+        self.assertEqual(h.tab[15][0][1], '123')
+
+    def testWithCollision(self):
+        h = HashTable(4)
+        h.put('pink' ,'123')
+        h.put('blue' ,'asd')
+        h.put('orange' ,'sad')
+        self.assertEqual(h.get('pink'), '123')
+        self.assertEqual(h.get('blue'), 'asd')
+        self.assertEqual(h.get('orange'), 'sad')
+        
+        self.assertEqual(h.tab[2][0][0], 'pink')
+        self.assertEqual(h.tab[2][0][1], '123')
+        
+        self.assertEqual(h.tab[2][1][0], 'blue')
+        self.assertEqual(h.tab[2][1][1], 'asd')
+        
+        self.assertEqual(h.tab[2][2][0], 'orange')
+        self.assertEqual(h.tab[2][2][1], 'sad')
+
+
+
+class TestHashFun(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(myHashFun('abc', 20), 6)
+        self.assertEqual(myHashFun('pink', 10), 0)
+        self.assertEqual(myHashFun('blue', 10), 0)
+        self.assertEqual(myHashFun('orange', 10), 0)
+        self.assertEqual(myHashFun('orangered', 10), 7)
+        self.assertEqual(myHashFun('cyan', 10), 3)
+
+    def test2(self):
+        self.assertEqual(myBetterHashFun('hello', 13), 7)
+        self.assertEqual(myBetterHashFun('goodbye', 13), 9)
+        self.assertEqual(myBetterHashFun('hi', 13), 10)
+        self.assertEqual(myBetterHashFun('cyan', 13), 5)
+        self.assertEqual(myBetterHashFun('pink', 13), 5)
+        
+
+if __name__ == "__main__":
+    unittest.main()
