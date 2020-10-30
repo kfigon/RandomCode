12444eaefa3a2d58dc1e6787fea6c550f81cb2c0
kfigon <kamil.figon@gmail.com>
2020-10-30 22:24:06 +0100

    remove node, remove connection

--- Algorytmy/udemyAlgorithms/graph/graphs.py
+++ Algorytmy/udemyAlgorithms/graph/graphs.py
@@ -6,6 +6,7 @@ class Graph:
     def __init__(self):
         self.data: Dict[str,List[str]] = {}
 
+# todo - toupper?
     def addNode(self, name: str):
         if name not in self.data:
             self.data[name] = []
@@ -14,7 +15,7 @@ class Graph:
         self._validate(nodeA, nodeB)
 
         self.data[nodeA].append(nodeB)
-        self.data[nodeB].append(nodeA)
+        self.data[nodeB].append(nodeA) # undirected part
         
     def areConnected(self, nodeA: str, nodeB: str) -> bool:
         self._validate(nodeA, nodeB)
@@ -27,5 +28,23 @@ class Graph:
         return False
 
     def _validate(self, nodeA: str, nodeB: str):
-        if self.data[nodeA] is None or self.data[nodeB] is None:
-            raise Exception(f'invalid nodes {nodeA}:{nodeB}')
+        self._validateSingle(nodeA)
+        self._validateSingle(nodeB)
+    
+    def _validateSingle(self, nodeA: str):
+        if self.data[nodeA] is None:
+            raise Exception(f'invalid nodes {nodeA}')
+
+    def removeConnection(self, nodeA:str, nodeB:str):
+        self._validate(nodeA, nodeB)
+        self.data[nodeA].remove(nodeB)
+        self.data[nodeB].remove(nodeA)
+
+    def removeNode(self, nodeA: str):
+        self._validateSingle(nodeA)
+        del self.data[nodeA]
+        
+        for k in self.data.keys():
+            if nodeA in self.data[k]:
+                self.data[k].remove(nodeA)
+                
--- Algorytmy/udemyAlgorithms/graph/testGraph.py
+++ Algorytmy/udemyAlgorithms/graph/testGraph.py
@@ -27,5 +27,37 @@ class Test(unittest.TestCase):
         g.addNode('Tokyo')
         self.assertRaises(Exception, g.connect, ('Tokyo', 'Warsaw'))
 
+    def testRemoveConnection(self):
+        g = Graph()
+        g.addNode('Tokyo')
+        g.addNode('New York')
+        g.addNode('Warsaw')
+        g.connect('Tokyo', 'New York')
+        g.connect('Warsaw', 'New York')
+
+        g.removeConnection('New York', 'Tokyo')
+
+        self.assertFalse(g.areConnected('Tokyo', 'New York'))
+        self.assertFalse(g.areConnected('New York', 'Tokyo'))
+        self.assertTrue(g.areConnected('New York', 'Warsaw'))
+
+    def testRemoveNode(self):
+        g = Graph()
+        g.addNode('Tokyo')
+        g.addNode('New York')
+        g.addNode('Warsaw')
+        
+        g.connect('Tokyo', 'New York')
+        g.connect('Warsaw', 'New York')
+        g.connect('Warsaw', 'Tokyo')
+
+        g.removeNode('New York')
+
+        self.assertRaises(Exception, g.areConnected, ('Tokyo', 'New York'))
+        self.assertRaises(Exception, g.areConnected, ('New York', 'Tokyo'))
+        self.assertRaises(Exception, g.areConnected, ('New York', 'Warsaw'))
+
+        self.assertTrue(g.areConnected('Warsaw', 'Tokyo'))
+
 if __name__ == "__main__":
     unittest.main()
