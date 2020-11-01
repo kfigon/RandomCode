041a3730ca3db3df2733760d6fea089282bea5dc
Kamil <kamil.figon@gmail.com>
2020-11-01 15:16:38 +0100

    first implementation of dijkstra

--- Algorytmy/udemyAlgorithms/graph/dijkstra.py
+++ Algorytmy/udemyAlgorithms/graph/dijkstra.py
@@ -1,6 +1,99 @@
-from typing import List
-from weightedGraph import Graph
+from typing import List, Tuple, Dict, Optional
+from weightedGraph import Graph, Connection
 
+class Previous:
+    def __init__(self):
+        self.data: Dict[str,str] = {}
+    
+    def markPrevious(self, nodeFrom: str, nodeTo: str):
+        self.data[nodeFrom] = nodeTo
+    def getConnections(self, nodeFrom: str, nodeTo: str) -> List[str]:
+        out: List[str] = []
+        n = nodeTo
+        while n != nodeFrom and n in self.data:
+            out.append(n)
+            n = self.data[n]
+        out.append(nodeFrom)
+        return out
+
+    def __repr__(self) -> str:
+        return str(self.data)
+
+class PathDiary:
+    def __init__(self, nodes: List[str], startingNode: str):
+        self.visited: List[str] = []
+
+        self.data: Dict[str, Optional[int]] = {}
+        for n in nodes:
+            self.data[n] = None # infinity
+        self.data[startingNode] = 0
+
+    def wasWisited(self, node: str) -> bool:
+        return node in self.visited
+
+    def markAsVisited(self, node: str):
+        self.visited.append(node)
+
+    def findSmallest(self) -> Optional[str]:
+        smallestDist = None
+        smallestKey = None
+        for key in self.data.keys():
+            dist = self.data[key]
+            if self.wasWisited(key) or dist is None:
+                continue
+            if smallestDist is None or dist < smallestDist:
+                smallestDist = dist
+                smallestKey = key
+        
+        return smallestKey
+    
+    def updatePath(self, node:str, path: int) -> bool:
+        if self.data[node] is None or path < self.data[node]:
+            self.data[node] = path
+            return True
+        return False
+
+    def getCumulatedPath(self, node:str) -> int:
+        if node not in self.data or self.data[node] is None:
+            return 0
+        return self.data[node]
+        
 class Dijkstra:
     def __init__(self, graph: Graph):
         self.graph = graph
+
+    def findPath(self, nodeFrom: str, nodeTo: str) -> Tuple[List[str],int]:
+        pathDiary = PathDiary(list(self.graph.data.keys()), nodeFrom)
+        previousMap = Previous()
+
+        analyzedNode = pathDiary.findSmallest()
+        while analyzedNode is not None:
+            # print()
+            pathDiary.markAsVisited(analyzedNode)
+
+            neighbours: List[Connection] = self.graph.data[analyzedNode]
+            for n in neighbours:
+                if pathDiary.wasWisited(n.name):
+                    continue
+
+                # print(f'analyzing {analyzedNode}, nei {n.name}')
+                v = n.weight + pathDiary.getCumulatedPath(analyzedNode)
+                if pathDiary.updatePath(n.name, v):
+                    # print(f'found smaller path for him, update to {pathDiary.getCumulatedPath(n.name)}')
+                    previousMap.markPrevious(n.name, analyzedNode)
+            # print(previousMap)
+            analyzedNode = pathDiary.findSmallest()
+
+        print(previousMap)
+        connectionsEndToStart = previousMap.getConnections(nodeFrom, nodeTo)
+        accumulatedLen = pathDiary.getCumulatedPath(nodeTo)
+        return list(reversed(connectionsEndToStart)), accumulatedLen
+
+        
+def createGraph(nodes: List[str], connections:List[Tuple[str,str,int]]):
+    g = Graph()
+    for n in nodes:
+        g.addNode(n)
+    for c in connections:
+        g.connect(c[0],c[1],c[2])
+    return g
--- Algorytmy/udemyAlgorithms/graph/testDijkstra.py
+++ Algorytmy/udemyAlgorithms/graph/testDijkstra.py
@@ -1,10 +1,28 @@
 import unittest
 from weightedGraph import Graph
-from dijkstra import Dijkstra
+from dijkstra import Dijkstra, createGraph
 
 class Test(unittest.TestCase):
+    def setUp(self):
+        nodes = ['A','B','C','D','E','F']
+        connections =[
+            ('A','B',4),
+            ('A','C',2), 
+            ('B','E',3), 
+            ('C','D',2), 
+            ('C','F',4), 
+            ('D','E',3), 
+            ('D','F',1),
+            ('F','E',1)
+        ]
+        g = createGraph(nodes, connections)
+        self.d = Dijkstra(g)
+
     def test1(self):
-        self.assertEqual(1,0)
+        self.assertEqual(self.d.findPath('A', 'E'), (['A','C','D','F','E'],6))
+        self.assertEqual(self.d.findPath('A', 'D'), (['A','C','D'],4))
+        self.assertEqual(self.d.findPath('F', 'C'), (['F','D','C'],3))
+        self.assertEqual(self.d.findPath('F', 'B'), (['F','E','B'],4))
 
 if __name__ == "__main__":
     unittest.main()
