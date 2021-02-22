a87cb8e95bce3c0863b531bd9fa690ffd60923d8
Kamil <kamil.figon@gmail.com>
2021-02-22 18:56:28 +0100

    picking nums done

--- _SPOJ/hackerRank/pickingNumbers/main.py
+++ _SPOJ/hackerRank/pickingNumbers/main.py
@@ -6,22 +6,21 @@ def findLongest(tab: List[int]) -> int:
 
     bottomIdx = 0
     maxLen = 0
-    for i in range(len(tab)-1):
+    minEl = tab[0]
+    for i in range(1,len(tab)):
         el = tab[i]
-        nextEl = tab[i+1]
 
-        if abs(el-nextEl) <= 1:
-            topIdx = i+1
+        if abs(minEl-el) <= 1:
+            maxLen = max(maxLen, i - bottomIdx + 1)
         else:
-            bottomIdx = i + 1
-            topIdx = bottomIdx
+            bottomIdx = i
+            minEl = tab[i]
 
-        maxLen = max(maxLen, topIdx-bottomIdx+1)
     print(f'{tab} -> {maxLen}')
     return maxLen
 
 assert findLongest([1,1,2,2,4,4,5,5,5]) == 5
-assert findLongest([4,6,5,3,3,1]) == 3
+assert findLongest([4,6,5,3,3,1]) == 3 # 1 3 3 4 5 6
 assert findLongest([1, 2, 2, 3, 1, 2]) == 5
 
 
