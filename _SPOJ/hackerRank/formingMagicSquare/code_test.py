828f5d37e5f2e61f2fc8ba1ef0f6cf8ea9332309
Kamil <kamil.figon@gmail.com>
2021-02-22 17:04:25 +0100

    pending task

--- _SPOJ/hackerRank/formingMagicSquare/code_test.py
+++ _SPOJ/hackerRank/formingMagicSquare/code_test.py
@@ -2,17 +2,26 @@ import unittest
 from code import *
 
 class TestParsing(unittest.TestCase):
-    def test1(self):
-        self.fail('todo')
+	def test1(self):
+		self.fail('todo')
 
 class Test(unittest.TestCase):
-    def test1(self):
-        matrix = [[5,3,4],[1,5,8],[6,4,2]]
-        got, cost = createMagicSquare(matrix)
-        expectedMagicSquare = [[8,3,4],[1,5,9],[6,7,2]]
-        expectedCost = 7
-        self.assertEqual(cost, expectedCost)
-        self.assertEqual(got, expectedMagicSquare)
+	def test1(self):
+		matrix = [[5,3,4],[1,5,8],[6,4,2]]
+		got, _ = createMagicSquare(matrix)
+		expectedMagicSquare = [[8,3,4],[1,5,9],[6,7,2]]
+		self.assertEqual(got, expectedMagicSquare)
+
+	def testCost(self):
+		cases = [
+			([[4,9,2], [3,5,7], [8,1,5]], 1),
+			([[4,8,2], [4,5,7], [6,1,1]], 4),
+			([[5,3,4],[1,5,8],[6,4,2]], 7),
+		]
+		for i,c in enumerate(cases):
+			with self.subTest(i):
+				got = createMagicSquare(c[0])
+				self.assertEqual(got, c[1])
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
