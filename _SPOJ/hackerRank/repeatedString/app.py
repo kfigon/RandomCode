e4de3ded8f207b82ac6324be7221f9366e931e2c
Kamil <kamil.figon@gmail.com>
2021-02-23 19:52:58 +0100

    #4 done

--- _SPOJ/hackerRank/repeatedString/app.py
+++ _SPOJ/hackerRank/repeatedString/app.py
@@ -1,8 +1,30 @@
 # https://www.hackerrank.com/challenges/repeated-string/problem
 
-def repeatedStr(pattern: str, limit: int) -> int:
-    return -1
+from typing import Set
 
+# find 'a'
+def repeatedStr(pattern: str, limit: int) -> int:
+    ids: Set[int] = set()
+    for i,c in enumerate(pattern):
+        if c == 'a':
+            ids.add(i)
+
+    totalPatternsInLimit = limit//len(pattern)
+    remainderIdx = (limit % len(pattern)) -1
+    howManyIdxsCought = 0
+    for i in ids:
+        if i <= remainderIdx:
+            howManyIdxsCought += 1
+    result = totalPatternsInLimit*len(ids) + howManyIdxsCought
+    print(f'{pattern} * {limit} -> {result}')
+    return result
+
+assert repeatedStr('abcac', 1) == 1
+assert repeatedStr('abcac', 2) == 1
+assert repeatedStr('abcac', 3) == 1
+assert repeatedStr('abcac', 4) == 2
+assert repeatedStr('abcac', 5) == 2
+assert repeatedStr('abcac', 6) == 3
 assert repeatedStr('abcac', 10) == 4
 assert repeatedStr('aba', 10) == 7
 assert repeatedStr('a', 1000000000000) == 1000000000000
