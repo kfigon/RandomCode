2b5c85bca1d4e6c89d1285eb576cff3b853cb576
Kamil <kamil.figon@gmail.com>
2021-02-23 19:34:13 +0100

    #2 done

---
+++ _SPOJ/hackerRank/repeatedString/app.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/repeated-string/problem
+
+def repeatedStr(pattern: str, limit: int) -> int:
+    return -1
+
+assert repeatedStr('abcac', 10) == 4
+assert repeatedStr('aba', 10) == 7
+assert repeatedStr('a', 1000000000000) == 1000000000000
+
+print('oks')
