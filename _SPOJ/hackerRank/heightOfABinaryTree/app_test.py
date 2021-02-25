f94e8ac5fd8f0fd8026ef179be4cccbc248f008d
Kamil <kamil.figon@gmail.com>
2021-02-25 18:20:55 +0100

    more more boiler

--- _SPOJ/hackerRank/heightOfABinaryTree/app_test.py
+++ _SPOJ/hackerRank/heightOfABinaryTree/app_test.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import unittest
 
 class Node:
     def __init__(self, val: int):
@@ -31,7 +31,15 @@ class Tree:
                 ptr = ptr.right
 
     def getHeight(self) -> int:
-        pass
+        height = 0
+        def traverse(n: Node):
+            if n is None:
+                return
+
+            traverse(n.left)
+            traverse(n.right)
+
+        return height
 
 
 def testTree(vals: List[int]) -> int:
@@ -40,9 +48,23 @@ def testTree(vals: List[int]) -> int:
         tree.insert(v)
     return tree.getHeight()
 
+class Test(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(testTree([]),0)
+    def test2(self):
+        self.assertEqual(testTree([431]), 0)
+    def test3(self):
+        self.assertEqual(testTree([4,2]), 1)
+    def test4(self):
+        self.assertEqual(testTree([4,2,6]), 1)
+    def test5(self):
+        self.assertEqual(testTree([4,2,6, 5]), 2)
+    def test6(self):
+        self.assertEqual(testTree([4,2,6, 5,1]), 2)
+    def test7(self):
+        self.assertEqual(testTree([4,2,6, 5,1,7]), 2)
+    def test8(self):
+        self.assertEqual(testTree([4,2,6, 5,1,7,8]), 3)
 
-assert testTree([]) == 0
-assert testTree([431]) == 0
-assert testTree([4,2]) == 1
-assert testTree([4,2,6]) == 1
-assert testTree([4,2,6, 5]) == 2
+if __name__ == '__main__':
+    unittest.main()
