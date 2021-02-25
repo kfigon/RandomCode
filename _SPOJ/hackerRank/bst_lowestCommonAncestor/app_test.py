d8b983035927c14b9544b22746d6926fcf3c0539
Kamil <kamil.figon@gmail.com>
2021-02-25 18:50:00 +0100

    lowest common ancestor done

--- _SPOJ/hackerRank/bst_lowestCommonAncestor/app_test.py
+++ _SPOJ/hackerRank/bst_lowestCommonAncestor/app_test.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 import unittest
 
 class Node:
@@ -9,7 +9,7 @@ class Node:
 
 class Tree:
     def __init__(self):
-        self.root = None
+        self.root: Node = None
 
     def insert(self, val: int):
         newNode = Node(val)
@@ -31,7 +31,17 @@ class Tree:
                 ptr = ptr.right
 
     def getLowestCommonAncestor(self, v1: int, v2: int) -> int:
+        node = self.root
+        while node:
+            if v1 < node.val and v2 < node.val:
+                node = node.left
+            elif v1 > node.val and v2 > node.val:
+                node = node.right
+            else:
+                return node.val
+
         return -1
+
 
 def testTree(vals: List[int], v1: int, v2: int) -> int:
     tree = Tree()
@@ -41,7 +51,7 @@ def testTree(vals: List[int], v1: int, v2: int) -> int:
 
 class Test(unittest.TestCase):
     def test1(self):
-        self.assertEqual(3, testTree([2,1,3,4,5,6], 4,6))
+        self.assertEqual(4, testTree([2,1,3,4,5,6], 4,6))
     def test2(self):
         self.assertEqual(4, testTree([4,2,3,1,7,6], 1,7))
 
