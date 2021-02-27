8953cc8541e25c7d4db029bdcbe9e151bf0bfb1d
Kamil <kamil.figon@gmail.com>
2021-02-27 17:49:28 +0100

    boiler done

--- Algorytmy/unstructuredAlgorithms/permutations/foo.py
+++ Algorytmy/unstructuredAlgorithms/permutations/foo.py
@@ -4,21 +4,25 @@
 # a,b,c: (n!) - can be built with a tree
 # [abc, acb, bac, bca, cab, cba]
 
+#                []
+#a              [a]
+#b        [ba]      [ab]
+#c    cba cba bac    cab acb abc
 
-from typing import List
 
-def generatePermutation(chars: str) -> List[str]:
+from typing import Set
+
+def generatePermutation(chars: str) -> Set[str]:
     if len(chars) == 0:
-        return ['']
-    return []
+        return set()
+    return set()
 
-def test(chars: str, expected: List[str]):
+def test(chars: str, expected: Set[str]):
     result = generatePermutation(chars)
-    assert len(result) == len(expected), f'for {chars} got {len(result)}, exp {len(expected)}'
-    for i in expected:
-        assert i in result, f'error in {chars}, {i} not found in {result}'
+    assert result == expected, f'{result} != {expected}'
 
-test('',[''])
-test('a',['','a'])
-test('abc',['','a','b','c','ab','ac','bc','abc'])
+test('',{''})
+test('a',{'a'})
+test('ab', {'ab', 'ba'})
+test('abc', {'abc','acb','cab', 'bac','bca','cba'})
 print('ok')
