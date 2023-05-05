5e9ae6f016db2fc91e96b1b35dd1b7b1d078222f
kfigon <kamil.figon@gmail.com>
2023-05-05 12:45:35 +0200

    normalise paths

--- Algorytmy/unstructuredAlgorithms/ElementsOfProgrammingInterviews/stacks_queues/test_stacks.py
+++ Algorytmy/unstructuredAlgorithms/ElementsOfProgrammingInterviews/stacks_queues/test_stacks.py
@@ -1,4 +1,5 @@
 import unittest
+from typing import List
 
 def matchingParents(parents: str) -> bool:
     stak = []
@@ -13,6 +14,18 @@ def matchingParents(parents: str) -> bool:
         elif len(c) == 0 or closing[stak.pop()] != c:
             return False
     return len(stak) == 0
+
+def normalisePath(inpt: str) -> str:
+    stak: List[str] = []
+    for token in inpt.split('/'):
+        if token == '' or token =='.':
+            continue
+        elif token == '..':
+            stak.pop()
+        else:
+            stak.append(token)
+    
+    return '/'.join(stak)
 
 class TestStack(unittest.TestCase):
     def testMatchingParenthesis(self):
@@ -31,6 +44,16 @@ class TestStack(unittest.TestCase):
         for d in data:
             with self.subTest(d[0]):
                 self.assertEqual(d[1], matchingParents(d[0]))
+
+    def testNormalisePath(self):
+        data = [
+            ('usr/lib/../bin/gcc', 'usr/bin/gcc'),
+            ('scripts//./../scripts/awk/././', 'scripts/awk'),
+            ('sc//./../tc/awk/././', 'tc/awk'),
+        ]
+        for d in data:
+            with self.subTest(d[0]):
+                self.assertEqual(d[1], normalisePath(d[0]))
 
 if __name__ == "__main__":
     unittest.main()
