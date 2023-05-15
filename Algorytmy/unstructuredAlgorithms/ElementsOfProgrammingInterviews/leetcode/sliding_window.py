251c6552daf89f7660d3f53cccc5db1616852d9c
Kamil <kamil.figon@gmail.com>
2023-05-15 23:34:49 +0200

    more grind

--- Algorytmy/unstructuredAlgorithms/ElementsOfProgrammingInterviews/leetcode/sliding_window.py
+++ Algorytmy/unstructuredAlgorithms/ElementsOfProgrammingInterviews/leetcode/sliding_window.py
@@ -84,4 +84,40 @@ def numOfSubarrays(arr: List[int], k: int, threshold: int) -> int:
         start+=1
             
     return out
-            
+
+# https://leetcode.com/problems/max-consecutive-ones-iii/
+def longestOnes(nums: List[int], k: int) -> int:
+    occ = {}
+    start = 0
+    longest = 0
+    for i,v in enumerate(nums):
+        occ[v] = occ.get(v,0)+1
+        
+        if occ.get(0,0) <= k:
+            longest = max(longest, i-start+1)
+        else:
+            while occ.get(0,0) > k:
+                occ[nums[start]]-=1
+                start+=1
+
+    return longest
+
+# https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length
+def maxVowels(s: str, k: int) -> int:
+    vowels = set(['a','e','i','o','u'])
+    currentVowels = 0
+    start = 0
+    count = 0
+    for i,c in enumerate(s):
+        if c in vowels:
+            currentVowels += 1
+
+        if i-start+1 != k:
+            continue # collect more
+        
+        count = max(currentVowels, count)
+        if s[start] in vowels:
+            currentVowels -=1
+        start += 1
+        
+    return count
